9cabf23753a1a1e4a7453477f91e9d1cdb453ebc
Alexandr Kucakov <JoobleWart@gmail.com>
2024-12-13 15:59:36 +0300

    11

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -27,7 +27,7 @@ posts = [
                 Мне всё думалось, что, останься мы на корабле, мы
                 непременно спаслись бы. Теперь из его обломков мы могли бы
                 построить баркас, на котором и выбрались бы из этого
-                гиблого места.''',},
+                гиблого места.''', },
     {
         'id': 2,
         'location': 'Остров отчаянья',
@@ -42,13 +42,16 @@ posts = [
     },
 ]
 
+
 def index(request):
-    inverted_posts = posts[::-1] 
+    inverted_posts = posts[::-1]
     return render(request, 'blog/index.html', {'posts': inverted_posts})
-    
+
+
 def post_detail(request, id):
     post = next((post for post in posts if post['id'] == id), None)
     return render(request, 'blog/detail.html', {'post': post})
+
 
 def category_posts(request, category_slug):
     return render(request, 'blog/category.html', {'category_slug': category_slug})
--- blogicum/blogicum/settings.py
+++ blogicum/blogicum/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pages.apps.PagesConfig',
-    'blog.apps.BlogConfig', 
+    'blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'blogicum.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'], 
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,7 +122,7 @@ USE_TZ = True
 STATIC_URL = '/templates/css/'
 STATICFILES_DIRS = [
     BASE_DIR / 'templates'
-    ]
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
